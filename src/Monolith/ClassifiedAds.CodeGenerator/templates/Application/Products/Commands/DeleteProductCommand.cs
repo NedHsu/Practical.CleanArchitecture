24b88ed6daf8fbd32cccd9384f107a83fc805768
Ned <Ned@DESKTOP-423GLQT>
2021-01-08 15:47:17 +0800

    Add code generator template

---
+++ src/Monolith/ClassifiedAds.CodeGenerator/templates/Application/Products/Commands/DeleteProductCommand.cs
@@ -0,0 +1,24 @@
+﻿using ClassifiedAds.Domain.Entities;
+
+namespace ClassifiedAds.Application.Products.Commands
+{
+    public class DeleteProductCommand : ICommand
+    {
+        public Product Product { get; set; }
+    }
+
+    internal class DeleteProductCommandHandler : ICommandHandler<DeleteProductCommand>
+    {
+        private readonly ICrudService<Product> _productService;
+
+        public DeleteProductCommandHandler(ICrudService<Product> productService)
+        {
+            _productService = productService;
+        }
+
+        public void Handle(DeleteProductCommand command)
+        {
+            _productService.Delete(command.Product);
+        }
+    }
+}
